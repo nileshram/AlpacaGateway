b14250cf9d69c8320556bfd0dbbc4429162527de
nileshram <nilesh.ram@hotmail.co.uk>
2020-10-13 18:45:44 +0100

    updates to gateway.py

--- src/connection/gateway.py
+++ src/connection/gateway.py
@@ -1,1 +1,22 @@
 # Created by nilesh at 09/10/2020
+
+class MarketDataAPI:
+
+    def __init__(self):
+        pass
+
+    def _authenticate(self):
+        pass
+
+    def _is_connected(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def disconnect(self):
+        pass
+
+    def subscribe(self, *args):
+        pass
+
