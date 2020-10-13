7da538fe2a751205214f133efa4ad81ca7888653
nileshram <nilesh.ram@hotmail.co.uk>
2020-10-13 18:41:35 +0100

    updates to handler.py

--- src/connection/handler.py
+++ src/connection/handler.py
@@ -1,1 +1,12 @@
 # Created by nilesh at 09/10/2020
+
+class ConnectionHandler:
+
+    def __init__(self):
+        pass
+
+    def _process(self):
+        pass
+
+    def process(self):
+        pass
